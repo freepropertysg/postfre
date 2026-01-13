29f7aa08987a96ddb44d6a2330ebafe67cb78327
freepropertysg <freepropertysg@gmail.com>
2026-01-13 11:05:56 +0800

    Update header.js

--- assets/header.js
+++ assets/header.js
@@ -104,7 +104,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <span style="font-weight:600;color:#2563EB;font-family:'Leckerli One',cursive;">P</span><span style="font-weight:600;color:#2563EB;">ost</span><span style="font-weight:600;color:#14B8A6;">Fre</span><span style="font-weight:400;color:#9CA3AF;">.com</span>
         </span>
         <span style="font-size:10.5px;font-weight:400;font-family:Poppins,sans-serif;color:#6B7280;">
-          Classified ads and listings
+          Classified Ads & Listings
         </span>
         <span style="font-size:11.5px;font-weight:400;font-family:Poppins,sans-serif;color:#4B5563;">
           Post it on PostFre
