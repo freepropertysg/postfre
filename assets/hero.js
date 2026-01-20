1878d23b91d8c11a27d2b43d1dff14eed55fce04
freepropertysg <freepropertysg@gmail.com>
2026-01-20 09:11:20 +0800

    Update hero.js

--- assets/hero.js
+++ assets/hero.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     <header class="hero">
       <h1>Classified Ads</h1>
       <p>Post ads for property, jobs, services, items for sale and more.</p>
-      <p><strong>Post your listing today. Email <a href="mailto:info@postfre.com">info@postfre.com</a></strong></p>
+      <p><strong>Post your ad today. Email <a href="mailto:info@postfre.com">info@postfre.com</a></strong></p>
     </header>
   `;
 });
