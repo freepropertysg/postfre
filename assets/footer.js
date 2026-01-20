ded230de1ea700856488c82671d4b22bee2952f0
freepropertysg <freepropertysg@gmail.com>
2026-01-20 20:11:48 +0800

    Update footer.js

--- assets/footer.js
+++ assets/footer.js
@@ -4,7 +4,7 @@ const footerHTML=`
 <footer style="text-align:center;font-size:0.9rem;">
 
   <p style="margin:0.2rem 0;">
-    © <span id="copyYear"></span> PostFre — Classifieds Platform
+    © <span id="copyYear"></span> PostFre — Classified Ads
   </p>
 
   <p style="margin:0.15rem 0;font-size:0.8rem;opacity:0.85;">
